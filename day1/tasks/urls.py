ef09ad5911ed132d55b4ec0661a0931027e90677
neun39 <s1ime.drei9@gmail.com>
2025-06-05 00:31:42 +0900

    task_createを追加

--- day1/tasks/urls.py
+++ day1/tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UncompletedTaskListView,CompletedTaskListView
+from .views import UncompletedTaskListView,CompletedTaskListView,TaskCreateView
 
 app_name = 'task'
 
 urlpatterns = [
     path('uncompleted/', UncompletedTaskListView.as_view(), name='task_list_uncompleted'),
     path('completed/', CompletedTaskListView.as_view(), name='task_list_completed'),
+    path('create/',TaskCreateView.as_view(),name="task_create")
 ]
